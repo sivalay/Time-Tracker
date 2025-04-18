e7dcc0473a5d2116dbc31c12d59496622356acec
Siva <sivalayavinod25@gmail.com>
2025-04-18 16:12:59 +0530

    time-block

--- ruf/script.js
+++ ruf/script.js
@@ -2,18 +2,21 @@
 const startButton = document.querySelector('.st-btn')
 const stopButton = document.querySelector('.ed-btn')
 const content = document.querySelector('.content-wrap')
+const timeContainer = document.querySelector('.time-wrap')
 
 let dates = []
 
+// function to get start Time
 
 function getStartTime(){
     const date = new Date()
-    if (dates.length == 0 || dates.at(-1).status == true){
+    if (dates.length == 0 || dates.at(-1).status == 'done'){
         let times = {
             start : `${date}`,
             end : '',
             id : '',
-            status : false
+            timeDiff : '',
+            status : 'processing'
         }
         dates.push(times)
         displayTime(dates)
@@ -23,7 +26,9 @@ function getStartTime(){
 }
 
 function displayTime(dates){
-    content.innerHTML =dates.map((dateItem, id) => {
+    timeContainer.innerHTML =dates.map((dateItem, id) => {
+
+        let endTimeHtml
 
         const newdate = new Date(dateItem.start)
         const hours = newdate.getHours()
@@ -31,13 +36,23 @@ function displayTime(dates){
         const secs = newdate.getSeconds()
         dateItem.id = id
 
+        if (dateItem.end != ''){
+            const endTime = new Date(dateItem.end)
+            const endHours = endTime.getHours()
+            const endMins = endTime.getMinutes()
+            const endSecs = endTime.getSeconds()
+            endTimeHtml = `${formatTime(endHours)} : ${formatTime(endMins)} : ${formatTime(endSecs)}`
+        }else {
+            endTimeHtml = '00 : 00 : 00'
+        }
+
         return `
         <li class="li" data-index="${id}">
             <div>Start Time : <span class="start-time" id="task${id}">${formatTime(hours)} : ${formatTime(mins)} : ${formatTime(secs)}</span></div>
 
-            <div>End Time : <span class="end-time" id="task${id}">00 : 00 : 00</span></div>
+            <div>End Time : <span class="end-time" id="task${id}">${endTimeHtml}</span></div>
 
-            <div class="result-el">Result : <span id="result"></span></div>
+            <div class="result-el">Time Duration : <span id="result">${dateItem.timeDiff}</span></div>
         </li>
     `
     }).join('')
@@ -46,12 +61,15 @@ function displayTime(dates){
 }
 
 
+// function to get runTime
 
 function getRunTime(dates){
-    console.log("hi")
+    // console.log("hi")
     const result = dates.map((dateItem, id) => {
-        console.log('hello')
-        if (dateItem.id == id){
+        // console.log('hello')
+        
+        if (dateItem.status === 'processing'){
+            displayTime(dates)
             const resultEl = document.querySelector('#result')
             const time = new Date(dateItem.start)
             const currentTime = new Date()
@@ -59,15 +77,20 @@ function getRunTime(dates){
             const hours = Math.floor(timeDiff / 3600 % 24)
             const mins = Math.floor(timeDiff / 60) % 60
             const seconds = Math.floor(timeDiff) % 60            
-            console.log(`${hours} : ${mins} : ${seconds}`)
-            resultEl.innerHTML = `${formatTime(hours)} : ${formatTime(mins)} : ${formatTime(seconds)}`
-            console.log(resultEl.innerHTML)
-            console.log('hi')
+            // console.log(`${hours} : ${mins} : ${seconds}`)
+            // if (dateItem.timeDiff == ''){
+            //     resultEl.innerHTML = `${formatTime(hours)} : ${formatTime(mins)} : ${formatTime(seconds)}`
+            // }
+            dateItem.timeDiff = `${formatTime(hours)} : ${formatTime(mins)} : ${formatTime(seconds)}`
+
+            console.log(dates)
+            // console.log(resultEl.innerHTML)
+            // console.log('hi')
         }
     }).join('')
     return result
 }
-// setInterval(getRunTime(dates), 1000)
+setInterval(getRunTime(dates), 1000)
 
 function formatTime(time){
     return time < 10 ? (`0${time}`) : time
@@ -78,11 +101,38 @@ startButton.addEventListener('click', (e) => {
     getStartTime()
     setInterval(getRunTime(dates), 1000)
 })
-// if(!dates.length == 0){
-//     setInterval(getRunTime(dates), 1000)
-// }
-// setInterval(getRunTime(dates), 1000)
 
+// function to get End Time
 
-// stopButton.addEventListener('click', getStartTime)
+function getEndTime(){
+    const date = new Date()
+    // const hours = date.getHours()
+    // const mins = date.getMinutes()
+    // const secs = date.getSeconds()
+    // console.log(dates,'first')
+    const r = dates.map((item, id) => {
+        if (item.status === 'processing'){
+            // const endTimeEl = document.querySelector('.end-time')
+            // endTimeEl.innerHTML = `${formatTime(hours)} : ${formatTime(mins)} : ${formatTime(secs)}`
+            // console.log(endTimeEl)
+            // const resultEl = document.querySelector('#result')
+            // console.log(resultEl.innerHTML)
+            // item.timeDiff = resultEl.innerHTML
+            item.end = `${date}`
+            item.status = 'done'
+            // displayTime(dates)
+            // setInterval(getRunTime(dates), 1000)
+            console.log(dates, 'after-end')
+            
+        }
+    })
+    // console.log(dates, 'second')
+}
+
+stopButton.addEventListener('click', () => {
+    setInterval(getRunTime(dates), 1000)
+    getEndTime()
+    displayTime(dates)
+    clearInterval()
+})
 
