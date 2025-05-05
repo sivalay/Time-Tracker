62ad5dc8c2c3e435068bb300555565c1a0b9001a
Siva <sivalayavinod25@gmail.com>
2025-05-05 16:46:33 +0530

    time-graph

--- daily-log/daily-log.js
+++ daily-log/daily-log.js
@@ -1,8 +1,6 @@
 import { tasks } from "../datas/tasks.js"
 
-
-
-const myCanvas = document.querySelector('#my-canva')
+// const myCanvas = document.querySelector('#mychart')
 const dateContainer = document.querySelector('.dates-container')
 const dateHead = document.querySelector('.today-head')
 const taskHead = document.querySelector('.time-log')
@@ -11,29 +9,23 @@ const display = document.querySelector('.output-log')
 const inputs = document.querySelector('#date-input')
 const doneBt = document.querySelector('#done-btn')
 
-// const mychart = new 
+
 
 const today = new Date()
 let weekDates = []
-const timeArray = []
+let taskArray = []
+let timeArray = []
+const rufTime = [6, 2]
+const rufTask = ['alksdj', 'lkjdoi']
+const backColor = ['red', 'green']
+const now = new Date()
 
-// console.log(tasks.map(task => task))
 
-// const ctx = myCanvas.getContext('2d')
-// console.log(ctx)
-
-// const todayDate = today.toLocaleDateString()
-// console.log(todayDate, "Today's Date")
-
-// doneBt.addEventListener('click', (e) => {
-//     e.preventDefault()
-//     console.log(inputs.value)
-//     const ip = inputs.value
-//     const date = new Date(ip)
-// })
 function displayEl(){
     getWeek()
     displayDates()
+    // displayTasks(now)
+    // location.reload()
 }
 displayEl()
 
@@ -41,12 +33,12 @@ displayEl()
 function getWeek(){
     const startDate = today
     startDate.setDate(startDate.getDate() - startDate.getDay())
-    console.log(startDate)
+    // console.log(startDate)
 
     for (let i=0; i<7; i++) {
         const current = new Date(startDate)
         current.setDate(current.getDate() + i)
-        console.log(current, 'current')
+        // console.log(current, 'current')
         const dates = {
                 date : current,
                 id : i
@@ -59,10 +51,8 @@ console.log(weekDates, "weekDates")
 
 // function to display Dates
 function displayDates(){
-    console.log("here")
-
     weekDates.map((dateItem) => {
-        console.log(dateItem.date)
+        // console.log(dateItem.date)
         const dateString = (new Date(dateItem.date)).toLocaleDateString()
 
         dateContainer.innerHTML += `
@@ -78,69 +68,143 @@ const dateEl = document.querySelectorAll('.date')
 dateEl.forEach((dateItem) => {
     dateItem.addEventListener('click', () => {
         const dateId = dateItem.dataset.dateId
-        // console.log(dateId, 'Here is the Id')
+        console.log(dateId, 'Here is the Id')
         let matchItem
         weekDates.map((weekDate) => {
             if (weekDate.id == dateId){
                 matchItem = weekDate
             }
         })
-        // console.log(matchItem, 'MatchItem')
+        console.log(matchItem, 'MatchItem')
         const matchDate = (matchItem.date).toLocaleDateString()
+        console.log(matchDate, 'matchDate')
         dateHead.innerHTML = matchDate
+        taskArray = []
+        timeArray = []
+        displayTasks(matchItem.date)
+        
     })
 })
 
 
 // first display of tasks for today
-function displayTasks(){
-    let timeArrayItem
-    const currentDate = (new Date()).toLocaleDateString()
-    console.log(currentDate, 'currentDate')
+function displayTasks(now){
+    let logDate
+    taskHead.innerHTML = ''
+    const currentDate = (new Date(now)).toLocaleDateString()
+    // console.log(currentDate, 'currentDate')
     tasks.map((task) => {    
         let diffSum = 0  
         task.timeLogs.map((timeItem) => {
-            const logDate = (new Date(`${timeItem.endLog}`)).toLocaleDateString()
+            logDate = (new Date(`${timeItem.endLog}`)).toLocaleDateString()
             // console.log(logDate, 'logDate')
-            if (currentDate == logDate){
+            if (currentDate === logDate){
                 
-                // console.log(task.timeLogs, task.tName)
-                // console.log(timeItem.newTimeDiff)
+                console.log(task.timeLogs, task.tName, 'here')
+                console.log(timeItem.newTimeDiff)
 
                 diffSum =diffSum + timeItem.newTimeDiff
             }
         })
         // console.log(diffSum, 'diffSum')
         if(diffSum != 0){
+            // diffSum = diffSum + timeItem.newTimeDiff
             const timeDiff = (diffSum) / 1000
-            console.log(timeDiff)
+            // console.log(timeDiff)
             const hours = Math.floor(timeDiff / 3600 % 24)
             const mins = Math.floor(timeDiff / 60) % 60
-            const seconds = (Math.floor(timeDiff) % 60) 
-            const timeDiffHtml = `${hours} : ${mins} : ${seconds}`
-            console.log(timeDiffHtml)
-            timeArrayItem = {
-                taskName : task.tName,
-                totalTime : diffSum
-            }
-            timeArray.push(timeArrayItem)
+            console.log(mins, 'mins')
+            // const seconds = (Math.floor(timeDiff) % 60) 
+            const timeDiffHtml = `${formatTime(hours)}.${formatTime(mins)}`
+            // console.log(timeDiffHtml)
+            timeArray.push(timeDiffHtml)
+            taskArray.push(task.tName)
         }
-        console.log(timeArray)
+        // diffSum = 0
+        console.log(timeArray, 'timeArray')
+        console.log(taskArray, 'taskArray')
 
     })
-    timeArray.map((timeItm) => {
+    console.log(taskArray.length, 'length')
+    if(taskArray.length === 0){
+        taskHead.innerHTML = `
+            <div class="task-container">
+                <div class="task-head">No Data On This Date</div>
+            </div>
+        `
+    }
+
+    taskArray.map((timeItm) => {
         taskHead.innerHTML += `
             <div class="task-container">
-                <div class="task-head">${timeItm.taskName}</div>
+                <div class="task-head">${timeItm}</div>
             </div>
         `
     })
+    displayChart()
 }
-displayTasks()
+displayTasks(now)
+
+// function to transform time
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : time
+}
+
+// function to get the touched date graph
+// function touchruf(now){
+//     const nowDate = (new Date(now)).toLocaleDateString()
+//     console.log(nowDate, 'now-date-string')
+//     tasks.map((taskItem) => {
+//         taskItem.timeLogs.map((log) => {
+//             const logDate = (new Date(`${log.endLog}`)).toLocaleDateString()
+//             console.log(logDate, 'now-date-fromlog')
+//             if (logDate === nowDate){
+//                 console.log(log, 'This is the log')
+//                 // console.log(logDate, "This is the log")
+//                 console.log(logDate)
+//                 // displayChart()
+//             }
+//         })
+//     })
+// }
+
+// displaying Chart.js
+function displayChart(){
+
+    new Chart("mychart", {
+        type : "bar",
+        data : {
+            labels : taskArray.map(task => task),
+            datasets : [{
+                backgroundColor : 'rgb(166, 173, 166)',
+                data : timeArray
+            }]
+        },
+        options : {
+            legent : {display: false},
+            scales : {
+                yAxes : [{
+                    ticks : {
+                        beginAtZero : true
+                    }
+                }]
+            },
+            title : {
+                display : true,
+                text : "Time Log"
+            }
+        }
+    })
+}
+displayChart()
 
 
 
-
+// function printingMsg(msg){
+//     console.log("The msg wanted to print")
+//     console.log("Here..")
+//     console.log(msg, 'var')
+// }
 
 
 // how to draw a graph in canvas with javascript!?
